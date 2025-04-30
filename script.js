938363568a8d3891e0e8dad61db65316a89c8842
vitorandrade222 <jg0112818@gmail.com>
2025-04-30 16:46:12 -0300

    mudança no script

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.ceil(document.querySelector(".input-max").value);
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (min >= max) {
+        alert("o valor min tem que ser menor que o valor max")
+    }
 
-    alert(result)
+    else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        alert("número sendo sorteado...........")
+        alert (result)
+    }
 
 }
